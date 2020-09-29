88509351f64cc0f8633b8f47b958c86d3d2f402f
Chavithra PARANA <chavithra@gmail.com>
2020-09-30 00:30:58 +0200

    update setup.py

--- setup.py
+++ setup.py
@@ -5,9 +5,10 @@ setup(
     packages=find_packages(),
     install_requires=[
         'click',
-        'grpcio',
         'requests',
         'wrapt',
+        'wheel',
+        'grpcio',
     ],
     author='Chavithra PARANA',
     author_email='chavithra@gmail.com',
