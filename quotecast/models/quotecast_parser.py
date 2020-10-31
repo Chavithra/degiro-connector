c574b61f0c9566c6a2fe6b897df6ad19dff0972e
Chavithra PARANA <chavithra@gmail.com>
2020-11-01 00:52:03 +0100

    improve metrics parsing : x100 faster

--- quotecast/models/quotecast_parser.py
+++ quotecast/models/quotecast_parser.py
@@ -4,6 +4,7 @@ import logging
 import quotecast.helpers.metric_handler as metric_handler
 
 from quotecast.models.metric_list_storage import MetricListSorage
+from quotecast.models.raw_metric_parser import RawMetricParser
 from quotecast.pb.quotecast_pb2 import (
     Matching,
     Metadata,
@@ -19,6 +20,31 @@ class QuotecastParser:
     It build Ticker object from it.
     """
 
+    @staticmethod
+    def extract_product_list(
+        metric_list,
+    )->List[int]:
+        metric_list = metric_list
+
+        product_list = list()
+        for metric in metric_list:
+            if not metric.product_id in product_list:
+                product_list.append(metric.product_id)
+
+        return product_list
+
+    @property
+    def ticker(self)->Ticker:
+        return self.__ticker
+
+    @property
+    def metric_list_storage(self)->MetricListSorage:
+        return self.__metric_list_storage
+
+    @property
+    def raw_metric_parser(self)->RawMetricParser:
+        return self.__raw_metric_parser
+
     def __init__(
         self,
         fill_na:bool=True,
@@ -32,42 +58,29 @@ class QuotecastParser:
     
         self.__logger = logging.getLogger(self.__module__)
         self.__fill_na = fill_na
-        self.__matching_list = None
         self.__ticker = None
         self.__metric_list_storage = MetricListSorage()
-
-    @property
-    def matching_list(self)->List[Matching]:
-        return self.__matching_list
-
-    @property
-    def ticker(self)->Ticker:
-        return self.__ticker
+        self.__raw_metric_parser = RawMetricParser()
 
     def put_quotecast(
         self,
         quotecast:Quotecast,
     ):
-        matching_list = self.__matching_list
         fill_na = self.__fill_na
         metric_list_storage = self.__metric_list_storage
+        raw_metric_parser = self.__raw_metric_parser
 
         metadata = quotecast.metadata
         json_data = quotecast.json_data
 
-        # SETUP MATCHING LIST & METRIC LIST
-        matching_list, metric_list = metric_handler.build_metric_list_from_json(
-            json_data=json_data,
-            matching_list=matching_list
-        )
+        # SETUP METRIC LIST        
+        raw_metric_parser.put_raw_metric_list(raw_json=json_data)
+        metric_list = raw_metric_parser.metric_list
 
         # SETUP PRODUCT LIST
-        product_list = metric_handler.extract_product_list(
-            metric_list=matching_list
+        product_list = self.extract_product_list(
+            metric_list=metric_list
         )
-
-        # SAVE MATCHING LIST
-        self.__matching_list = matching_list
 
         # SETUP FILLED METRIC LIST
         if fill_na == True:
