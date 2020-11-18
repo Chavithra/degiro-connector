d63e7e3198880b8728e3a791c41f41dfe2d6d02f
Chavithra PARANA <chavithra@gmail.com>
2020-11-18 05:01:43 +0100

    update quotecast

--- quotecast/models/metrics_storage.py
+++ quotecast/models/metrics_storage.py
@@ -3,10 +3,14 @@ import quotecast.helpers.pb_handler as pb_handler
 from quotecast.pb.quotecast_pb2 import Ticker
 
 class MetricsStorage:
+    @property
+    def storage_ticker(self)->Ticker:
+        return self.__storage_ticker
+
     def __init__(self):
         self.__storage_ticker = Ticker()
     
-    def __add_metrics(self, ticker:Ticker):
+    def add_metrics(self, ticker:Ticker):
         storage_ticker = self.__storage_ticker
         pb_handler.merge_tickers(
             ticker1=storage_ticker,
@@ -17,7 +21,7 @@ class MetricsStorage:
     def fill_ticker(self, ticker:Ticker):
         storage_ticker = self.__storage_ticker
 
-        self.__add_metrics(ticker=ticker)
+        self.add_metrics(ticker=ticker)
             
         pb_handler.merge_tickers(
             ticker1=ticker,
