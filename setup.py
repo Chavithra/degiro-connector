221e5de1a5c747dc5e92a49de9e9ebdfc90a3b58
Chavithra PARANA <chavithra@gmail.com>
2021-06-22 21:22:26 +0200

    setup : update version

--- setup.py
+++ setup.py
@@ -35,5 +35,5 @@ setup(
     packages=find_packages(include=["degiro_connector*"]),
     python_requires='>=3.6',
     url='https://github.com/chavithra/degiro-connector',
-    version='1.0.1',
+    version='1.0.2',
 )
