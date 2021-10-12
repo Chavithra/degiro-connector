fb702805ba42ffeae19fde338f533c6b321a9a00
Chavithra PARANA <chavithra@gmail.com>
2021-10-12 23:32:22 +0200

    core : add helper method

--- degiro_connector/core/helpers/pb_handler.py
+++ degiro_connector/core/helpers/pb_handler.py
@@ -1,11 +1,15 @@
 # IMPORTATION STANDARD
+from typing import Dict
+
 # IMPORTATION THIRD PARTY
-# IMPORTATION INTERNAL
 from google.protobuf import json_format
 from google.protobuf.message import Message
+from google.protobuf.struct_pb2 import Struct
+
+# IMPORTATION INTERNAL
 
 
-def message_to_dict(message: Message) -> dict:
+def message_to_dict(message: Message) -> Dict:
     return json_format.MessageToDict(
         message=message,
         including_default_value_fields=True,
@@ -13,4 +17,13 @@ def message_to_dict(message: Message) -> dict:
         use_integers_for_enums=True,
         descriptor_pool=None,
         float_precision=None,
+    )
+
+
+def struct_from_dict(json_dict: Dict) -> Struct:
+    return json_format.ParseDict(
+        js_dict=json_dict,
+        message=Struct(),
+        ignore_unknown_fields=True,
+        descriptor_pool=None,
     )
