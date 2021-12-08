42742e883fdb453a43f80d629621b173f37ec34a
Chavithra PARANA <chavithra@gmail.com>
2021-12-08 06:36:28 +0100

    Trading : example qrcode

--- examples/trading/qrcode.py
+++ examples/trading/qrcode.py
@@ -3,7 +3,9 @@ from PIL import Image
 
 """
     This is an example on how to extract the text from a QRCode image.
-    You will need to install the library : pyzbar
+    You will need to install the following libraries :
+        - pyzbar
+        - Pillow
 """
 
 data = decode(Image.open("YOUR_QRCODE.png"))[0].data
