03500beef81aac9db648bc622e15f60ba462ad2d
Chavithra PARANA <chavithra@gmail.com>
2024-01-02 00:48:18 +0100

    examples

--- examples/trading/order.py
+++ examples/trading/order.py
@@ -3,7 +3,7 @@ import logging
 
 from degiro_connector.trading.api import API as TradingAPI
 from degiro_connector.trading.models.credentials import Credentials
-from degiro_connector.trading.models.trading_pb2 import Order
+from degiro_connector.trading.models.order import Action, Order, OrderType, TimeType
 
 logging.basicConfig(level=logging.DEBUG)
 
@@ -11,40 +11,24 @@ with open("config/config.json") as config_file:
     config_dict = json.load(config_file)
 
 credentials = Credentials.model_validate(obj=config_dict)
-
-# SETUP TRADING API
 trading_api = TradingAPI(credentials=credentials)
-
-# ESTABLISH CONNECTION
 trading_api.connect()
 
-# SETUP ORDER
+# PASS ORDER
 order = Order(
-    action=Order.Action.BUY,
-    order_type=Order.OrderType.LIMIT,
-    price=10,
-    product_id=71981,
+    buy_sell=Action.BUY,
+    order_type=OrderType.LIMIT,
+    price=12.1,
+    product_id=72160,
     size=1,
-    time_type=Order.TimeType.GOOD_TILL_DAY,
+    time_type=TimeType.GOOD_TILL_DAY,
 )
 
-# FETCH CHECKING_RESPONSE
 checking_response = trading_api.check_order(order=order)
-
-# EXTRACT CONFIRMATION_ID
-confirmation_id = checking_response.confirmation_id
-
-# EXTRACT OTHER DATA
-free_space_new = checking_response.free_space_new
-response_datetime = checking_response.response_datetime
-transaction_fees = checking_response.transaction_fees
-transaction_opposite_fees = checking_response.transaction_opposite_fees
-transaction_taxes = checking_response.transaction_taxes
-
-# SEND CONFIRMATION
-confirmation_response = trading_api.confirm_order(
-    confirmation_id=confirmation_id, order=order
-)
-
 print(checking_response)
+
+confirmation_response = trading_api.confirm_order(
+    confirmation_id=checking_response.confirmation_id,
+    order=order,
+)
 print(confirmation_response)
