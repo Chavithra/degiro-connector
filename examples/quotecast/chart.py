84ab99009b7097c7488ba1f9f724cbe89586e792
Chavithra PARANA <chavithra@gmail.com>
2024-01-01 15:57:42 +0100

    examples

--- examples/quotecast/chart.py
+++ examples/quotecast/chart.py
@@ -1,22 +1,20 @@
 import json
 import logging
 
+import polars as pl
+from pydantic import BaseModel
+
 from degiro_connector.quotecast.tools.chart_fetcher import ChartFetcher
 from degiro_connector.quotecast.models.chart import ChartRequest, Interval
 
-# SETUP LOGGING
 logging.basicConfig(level=logging.INFO)
 
 with open("config/config.json") as config_file:
     config_dict = json.load(config_file)
-
-
 user_token = config_dict.get("user_token")  # HERE GOES YOUR USER_TOKEN
 
-# SETUP API
+# FETCH CHART
 chart_fetcher = ChartFetcher(user_token=user_token)
-
-# PREPARE REQUEST
 chart_request = ChartRequest(
     culture="fr-FR",
     # override={
@@ -28,9 +26,9 @@ chart_request = ChartRequest(
     resolution=Interval.PT1H,
     series=[
         "issueid:360148977",
-        # "price:issueid:360148977",
-        # "ohlc:issueid:360148977",
-        # "volume:issueid:360148977",
+        "price:issueid:360148977",
+        "ohlc:issueid:360148977",
+        "volume:issueid:360148977",
         # "vwdkey:AAPL.BATS,E",
         # "price:vwdkey:AAPL.BATS,E",
         # "ohlc:vwdkey:AAPL.BATS,E",
@@ -38,12 +36,14 @@ chart_request = ChartRequest(
     ],
     tz="Europe/Paris",
 )
-
-# FETCH DATA
 chart = chart_fetcher.get_chart(
     chart_request=chart_request,
-    raw=True,
+    raw=False,
 )
 
-# DISPLAY
-print(chart)
+if isinstance(chart, BaseModel):
+    for series in chart.series:
+        print(pl.DataFrame(data=series.data, orient="row"))
+
+if isinstance(chart, dict):
+    print(chart)
