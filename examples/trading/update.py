e9691a901df1e9e744b9d4e0c87d08c708d5e698
Chavithra PARANA <chavithra@gmail.com>
2024-01-02 20:04:19 +0100

    examples

--- examples/trading/update.py
+++ examples/trading/update.py
@@ -15,38 +15,39 @@ credentials = Credentials.model_validate(obj=config_dict)
 trading_api = TradingAPI(credentials=credentials)
 trading_api.connect()
 
-request_list = [
-    UpdateRequest(
-        option=UpdateOption.ALERTS,
-        last_updated=0,
-    ),
-    UpdateRequest(
-        option=UpdateOption.CASH_FUNDS,
-        last_updated=0,
-    ),
-    UpdateRequest(
-        option=UpdateOption.HISTORICAL_ORDERS,
-        last_updated=0,
-    ),
-    UpdateRequest(
-        option=UpdateOption.ORDERS,
-        last_updated=0,
-    ),
-    UpdateRequest(
-        option=UpdateOption.PORTFOLIO,
-        last_updated=0,
-    ),
-    UpdateRequest(
-        option=UpdateOption.TOTAL_PORTFOLIO,
-        last_updated=0,
-    ),
-    UpdateRequest(
-        option=UpdateOption.TRANSACTIONS,
-        last_updated=0,
-    ),
-]
-
 # FETCH ACCOUNT UPDATE
-account_update = trading_api.get_update(request_list=request_list, raw=True)
+account_update = trading_api.get_update(
+    request_list=[
+        UpdateRequest(
+            option=UpdateOption.ALERTS,
+            last_updated=0,
+        ),
+        UpdateRequest(
+            option=UpdateOption.CASH_FUNDS,
+            last_updated=0,
+        ),
+        UpdateRequest(
+            option=UpdateOption.HISTORICAL_ORDERS,
+            last_updated=0,
+        ),
+        UpdateRequest(
+            option=UpdateOption.ORDERS,
+            last_updated=0,
+        ),
+        UpdateRequest(
+            option=UpdateOption.PORTFOLIO,
+            last_updated=0,
+        ),
+        UpdateRequest(
+            option=UpdateOption.TOTAL_PORTFOLIO,
+            last_updated=0,
+        ),
+        UpdateRequest(
+            option=UpdateOption.TRANSACTIONS,
+            last_updated=0,
+        ),
+    ],
+    raw=True,
+)
 
 print(account_update)
